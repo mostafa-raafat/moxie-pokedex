33599204f00eac5b3f7669917c58f8be477fc53e
Mostafa Ismail <mraafatsaad94@gmail.com>
2024-05-23 00:57:39 +0200

    feat: setup react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
-  return <div>MOXIE-POKEDEX</div>;
-}
+// Pages
+import HomePage from "./pages/home";
+import PokemonDetailPage from "./pages/pokemon";
+
+// Components
+import NotFound from "./components/NotFound/NotFound";
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/pokemon/:name" element={<PokemonDetailPage />} />
+      <Route path="*" element={<NotFound />} /> {/* Catch-all route for 404 */}
+    </Routes>
+  </Router>
+);
 
 export default App;
